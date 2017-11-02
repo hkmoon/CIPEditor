3be4cf6820ef6630b6da804dcf12c683fafb96bb
HongKee Moon <hkmoon@me.com>
2017-11-02 17:02:22 +0100

    Initial commit

---
+++ src/main/java/invizio/cip/CIPEditor.java
@@ -0,0 +1,10 @@
+package invizio.cip;
+
+/**
+ * Author: HongKee Moon (moon@mpi-cbg.de), Scientific Computing Facility
+ * Organization: MPI-CBG Dresden
+ * Date: November 2017
+ */
+public class CIPEditor
+{
+}
